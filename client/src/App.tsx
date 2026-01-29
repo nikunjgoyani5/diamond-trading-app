f73056ed04c7ff21c93e75eb14311836d2726d3e
vidhidoctor26 <vidhidoctor@logicgoinfotech.in>
2026-01-29 09:54:42 +0530

    Initial project setup with client structure

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center text-red-500">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
